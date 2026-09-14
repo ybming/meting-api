59fa2bd9dfbb9c532db6fd8a7814ddaa3753affa
ybming <88264029@qq.com>
2026-09-14 20:00:14 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -470,7 +470,7 @@ app.get('/', (c) => {
             <h1>Meting API</h1>
             <p>多平台音乐 API 服务</p>
             <div class="badges">
-                <a href="https://github.com/mikus-loli/Meting-API" style="text-decoration:none;">
+                <a href="https://github.com/ybming/Meting-API" style="text-decoration:none;">
                     <img alt="Github" src="https://img.shields.io/badge/Github-Meting-green">
                     <img alt="Forks" src="https://img.shields.io/github/forks/mikus-loli/Meting-API">
                     <img alt="Stars" src="https://img.shields.io/github/stars/mikus-loli/Meting-API">
@@ -527,7 +527,7 @@ app.get('/', (c) => {
                     </span>
                     <span class="link-arrow">→</span>
                 </a>
-                <a class="link-card" href="https://github.com/mikus-loli/Meting-API" target="_blank">
+                <a class="link-card" href="https://github.com/ybming/Meting-API" target="_blank">
                     <span class="link-icon">📖</span>
                     <span class="link-info">
                         <span class="link-title">项目文档</span>
@@ -674,7 +674,7 @@ Location: https://img.example.com/cover.jpg</pre>
 
         </div>
     </div>
-    <footer>Powered by <a href="https://github.com/mikus-loli/Meting-API" target="_blank">Meting-API</a></footer>
+    <footer>Powered by <a href="https://github.com/ybming/Meting-API" target="_blank">Meting-API</a></footer>
     <script>
     function copyCode(btn) {
         const pre = btn.parentElement.querySelector('pre');
